e2964e7078f27a48c6da36d602d2767e6c316073
amkeyte <amkeyte@gmail.com>
2025-05-27 15:54:48 -0700

    20250527-1554

---
+++ src/LunaFlow.Rules/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace LunaFlow.Rules
+{
+    /// <summary>
+    /// Placeholder for DocFx
+    /// </summary>
+    internal class Class1
+    {
+        /// <summary>
+        /// who cares
+        /// </summary>
+        public static string boo = "boo";
+    }
+}
